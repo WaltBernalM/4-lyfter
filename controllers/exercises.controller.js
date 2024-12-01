93b35a3e37937b1c21edc6675528c2a33a14e8d8
Walter Bernal <wltrbm@gmail.com>
2024-11-30 18:20:29 -0700

    feat: exercise search query by name

--- controllers/exercises.controller.js
+++ controllers/exercises.controller.js
@@ -1,63 +1,69 @@
 // @ts-check
 
 import Exercise from '../models/Exercise.model.js'
-import mongoose from 'mongoose'
 import { populateExerciseDb } from '../utils/seeds.utils.js'
 
 export const getAllExercises = async (req, res, next) => {
   try {
     const { query } = req
+    const { type, muscle, name } = query
 
-    if (query) {
-      const { type } = query
-      const { muscle } = query
-      const types = [
-        "cardio",
-        "omympic_weightlifting",
-        "polymetrics",
-        "powerlifting",
-        "strength",
-        "stretching",
-        "strongman",
-      ]
-      const muscles = [
-        "abdominals",
-        "abductors",
-        "adductors",
-        "biceps",
-        "calves",
-        "chest",
-        "forearms",
-        "glutes",
-        "hamstrings",
-        "lats",
-        "lower_back",
-        "middle_back",
-        "neck",
-        "quadriceps",
-        "traps",
-        "triceps",
-      ]
+    const types = [
+      "cardio",
+      "olympic_weightlifting",
+      "plyometrics",
+      "powerlifting",
+      "strength",
+      "stretching",
+      "strongman",
+    ]
+    const muscles = [
+      "abdominals",
+      "abductors",
+      "adductors",
+      "biceps",
+      "calves",
+      "chest",
+      "forearms",
+      "glutes",
+      "hamstrings",
+      "lats",
+      "lower_back",
+      "middle_back",
+      "neck",
+      "quadriceps",
+      "traps",
+      "triceps",
+    ]
 
-      if (type && !types.includes(type)) {
-        res.status(400).json({ message: "Invalid query parameter" })
-        return
-      } else if (muscle && !muscles.includes(muscle)) {
-        res.status(400).json({ message: "Invalid query parameter" })
-        return
-      }
+    if (type && !types.includes(type)) {
+      return res.status(400).json({ message: "Invalid 'type' query parameter" })
     }
 
-    const allExercisesInDB = await Exercise.find()
-    if (allExercisesInDB.length === 0) {
+    if (muscle && !muscles.includes(muscle)) {
+      return res
+        .status(400)
+        .json({ message: "Invalid 'muscle' query parameter" })
+    }
+
+    let searchQuery = {}
+    if (type) searchQuery.type = type
+    if (muscle) searchQuery.muscle = muscle
+    if (name) searchQuery.name = { $regex: name, $options: 'i' }
+
+    let allExercises = await Exercise.find(searchQuery)
+    if (allExercises.length === 0) {
       await populateExerciseDb()
+      allExercises = await Exercise.find(searchQuery)
     }
 
-    const allExercises = await Exercise.find(query)
-    res
-      .status(200)
-      .json({ allExercises })
+    res.status(200).json({ allExercises })
   } catch (error) {
-    res.status(500).json({ message: "Exercises Controller: Internal Server Error", error: error.message })
+    res
+      .status(500)
+      .json({
+        message: "Exercises Controller: Internal Server Error",
+        error: error.message,
+      })
   }
 }
